f8a60198c89a7ce5b47ce5c58ed86572043221e3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-10-06 16:57:50 -0700

    Updated the schema source.

--- inst/source.sh
+++ inst/source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://artifactdb.github.io/BiocObjectSchemas/bundle.tar.gz > bundle.tar.gz
+curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2022-10-06/bundle.tar.gz > bundle.tar.gz
 rm -rf schemas
 tar -xvf bundle.tar.gz
 mv resolved schemas
