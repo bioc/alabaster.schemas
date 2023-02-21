31d772e2d1e363fd068dc112a571f996581881ec
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-20 21:58:52 -0800

    Updated schemas.

--- inst/source.sh
+++ inst/source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2022-10-18/bundle.tar.gz > bundle.tar.gz
+curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2023-02-20/bundle.tar.gz > bundle.tar.gz
 rm -rf schemas
 tar -xvf bundle.tar.gz
 mv resolved schemas
