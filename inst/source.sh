2e3da112e1fea92d6e6f2b2090a885ee4bdde550
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-04-14 14:04:12 -0700

    Updated schemas for date-time support.

--- inst/source.sh
+++ inst/source.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2023-02-20/bundle.tar.gz > bundle.tar.gz
+curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2023-04-14/bundle.tar.gz > bundle.tar.gz
+# curl -L https://artifactdb.github.io/BiocObjectSchemas/bundle.tar.gz > bundle.tar.gz # for testing the latest.
 rm -rf schemas
 tar -xvf bundle.tar.gz
 mv resolved schemas
