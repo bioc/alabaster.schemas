799d5be9d8cb37a9b85174a7bc90e243ba2484d3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-07 00:24:36 -0700

    Updated schemas again, bumped versions.

--- inst/source.sh
+++ inst/source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2023-04-14/bundle.tar.gz > bundle.tar.gz
+curl -L https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2023-05-07/bundle.tar.gz > bundle.tar.gz
 # curl -L https://artifactdb.github.io/BiocObjectSchemas/bundle.tar.gz > bundle.tar.gz # for testing the latest.
 rm -rf schemas
 tar -xvf bundle.tar.gz
